787caa050b37aba41a4a1d12e30ea8feb24ebc40
Srajan Shrivastava <121538686+Srajan-02@users.noreply.github.com>
2023-12-08 14:41:45 +0530

    Add files via upload

---
+++ spam_detection/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+import pickle
+model = 'model.pkl'
+classifier = pickle.load(open(model, 'rb'))
+tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        message = request.form['message']
+        data = [message]
+        vect = tfidf.transform(data).toarray()
+        my_prediction = classifier.predict(vect)
+        return render_template('result.html', prediction=my_prediction)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
